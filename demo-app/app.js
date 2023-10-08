6289b7a9ad899c53c3021d9a1b70fb87a295ee2c
shreyanshs7 <shreyanshss7@gmail.com>
2023-10-08 19:59:01 +0530

    deploying new changes

--- demo-app/app.js
+++ demo-app/app.js
@@ -8,4 +8,5 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
+  console.log('Deployed new changes');
 });

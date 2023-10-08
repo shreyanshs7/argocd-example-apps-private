fe7fa723b538639c2f3048c39cafb5b594414b03
shreyanshs7 <shreyanshss7@gmail.com>
2023-10-08 16:12:34 +0530

    added new app

---
+++ demo-app/app.js
@@ -0,0 +1,11 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, Docker World!');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
